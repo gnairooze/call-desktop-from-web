b4ef48e99704504ce7aeb75b122bca983c40fa85
George Nairooze <george.nairooze@linkdev.com>
2023-08-30 16:57:07 +0300

    add setup batch file

---
+++ code/CallFromWeb/setup-reg.bat
@@ -0,0 +1,6 @@
+﻿set key=callfromweb 
+reg add HKCR\%key% /ve /d "URL:Description" 
+reg add HKCR\%key% /v "URL Protocol" /d "" 
+reg add HKCR\%key%\shell 
+reg add HKCR\%key%\shell\open 
+reg add HKCR\%key%\shell\open\command /ve /d ""c:\path to\your.exe" ""%%1"""
